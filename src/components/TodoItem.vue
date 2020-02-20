83ae2be6c0b8a5bcc7736e7e65771bf102283874
Andreea Hanson <andreea.hanson7@gmail.com>
2020-02-20 14:39:37 -0700

    Add events and delete method

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,33 +1,42 @@
 <template>
-  <div>
-    <p>{{todo.title}}</p>
+  <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">
+    <li>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+    </li>
   </div>
 </template>
 
 <script>
 export default {
   name: "TodoItem",
-  props: ["todo"]
-}
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .todo-item {
-    background: #f4f4f4;
-    padding: 10px;
-    border-bottom: 1px #ccc dotted;
-  }
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
 
-  .is-complete {
-    text-decoration: line-through;
-  }
+.is-complete {
+  text-decoration: line-through;
+}
 
-  .del {
-    background: #ff0000;
-    color: #fff;
-    border: none;
-    padding: 5px 9px;
-    cursor: pointer;
-    float: right;
-  }
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  cursor: pointer;
+  float: right;
+}
 </style>
